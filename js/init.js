f79afe65a1f2aa62fc90bb2cd1259b516c2ed358
JohnnyT <johnnyt@xan.do>
2011-09-23 12:40:11 -0600

    Added ability to store and load packages from HTML5 local storage.
    Shortened button text of package buttons, added 'Local' button which
    will commit the selected package to
    localStorage['smalltalk.packages.NAME'].
    Refactored amber.js and created a JS Amber object.
    As part of Amber's startup process, all packages stored in
    localStorage['smalltalk.packages.*'] will be loaded.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,1 @@
-smalltalk.init(smalltalk.Object);
-smalltalk.classes()._do_(function(each) {each._initialize()});
-
-/* Similar to jQuery(document).ready() */
-
-if(this.smalltalkReady) {
-    this.smalltalkReady();
-}
+Amber.initializeSmalltalk();
