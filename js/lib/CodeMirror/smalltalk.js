f689e8e0f2a20b8126bd1540e34873ae6f121772
JohnnyT <johnnyt@moneydesktop.com>
2012-11-03 15:31:17 -0600

    Set CodeMirror indentUnit to 4

--- js/lib/CodeMirror/smalltalk.js
+++ js/lib/CodeMirror/smalltalk.js
@@ -1,6 +1,6 @@
 CodeMirror.defineMode('smalltalk', function(config, modeConfig) {
 	config.indentWithTabs = true;
-	config.tabSize = 2;
+	config.indentUnit = 4;
 
 	var specialChars = /[+\-/\\*~<>=@%|&?!.:;^]/;
 	var keywords = /true|false|nil|self|super|thisContext/;
