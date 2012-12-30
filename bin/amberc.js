7bce0e289130b9bab6deab49024d388858fb094a
Manfred Kroehnert <mkroehnert@users.sourceforge.net>
2012-12-30 01:58:23 +0100

    amberc.js: add Combo 'library'
    taken from http://howtonode.org/control-flow

--- bin/amberc.js
+++ bin/amberc.js
@@ -21,6 +21,30 @@ function map(array, filter, callback) {
 	});
 }
 
+function Combo(callback) {
+  this.callback = callback;
+  this.items = 0;
+  this.results = [];
+}
+
+Combo.prototype = {
+  add: function () {
+    var self = this,
+        id = this.items;
+    this.items++;
+    return function () {
+      self.check(id, arguments);
+    };
+  },
+  check: function (id, arguments) {
+    this.results[id] = Array.prototype.slice.call(arguments);
+    this.items--;
+    if (this.items == 0) {
+      this.callback.apply(this, this.results);
+    }
+  }
+};
+
 console.time('Compile Time');
 
 var defaults = function() {
