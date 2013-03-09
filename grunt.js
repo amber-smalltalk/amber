245a5d47200541d3f439aa1e0c143176946c6fea
Manfred Kroehnert <mkroehnert@users.sourceforge.net>
2013-03-09 13:19:05 +0100

    grunt: rename output of amber_dev target and remove Compiler.js

--- grunt.js
+++ grunt.js
@@ -70,11 +70,11 @@ module.exports = function(grunt) {
         working_dir: 'js',
         src: ['Kernel-Objects.js', 'Kernel-Classes.js', 'Kernel-Methods.js', 'Kernel-Collections.js',
               'Kernel-Exceptions.js', 'Kernel-Transcript.js', 'Kernel-Announcements.js',
-              'Compiler.js', 'Compiler-Exceptions.js', 'Compiler-Core.js', 'Compiler-AST.js',
+              'Compiler-Exceptions.js', 'Compiler-Core.js', 'Compiler-AST.js',
               'Compiler-IR.js', 'Compiler-Inlining.js', 'Compiler-Semantic.js',
               'Kernel-Tests.js', 'Compiler-Tests.js',
               'Canvas.js', 'IDE.js', 'SUnit.js', 'Documentation.js'],
-        output_name: 'amber_lib'
+        output_name: 'amber_dev'
       },
       server: {
         working_dir: 'server',
