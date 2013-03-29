191a17db236b16d84f691adabf97878c2c0a7edb
Herbert Vojčík <herby@mailbox.sk>
2013-03-29 16:11:09 +0100

    init.js commented

--- js/init.js
+++ js/init.js
@@ -11,8 +11,16 @@
     }
 
     if (inBrowser) {
+        // init is lengthy process done in JavaScript.
+        // setTimeout here postpones it, so DOM ready
+        // event can occur sooner, thus load process
+        // may appear optically faster.
         setTimeout(init, 0);
     } else {
+        // In certain configurations, setTimeout is not feasible.
+        // It is mainly for `amberc`-produced concatenated
+        // node.js programs. There, the actual "main" appears
+        // immediately after init, so it must happens synchronously.
         init();
     }
 })();
