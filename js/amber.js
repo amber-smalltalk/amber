91c55678351408351d0a04797dd15b02fcc7922c
Herbert Vojčík <herby@mailbox.sk>
2013-07-20 00:44:35 +0200

    renaming lib to support

--- js/amber.js
+++ js/amber.js
@@ -51,8 +51,8 @@ amber = (function() {
 		}
 
 		loadDependencies();
-		addJSToLoad('lib/es5-shim-2.0.2/es5-shim.min.js');
-		addJSToLoad('lib/es5-shim-2.0.2/es5-sham.min.js');
+		addJSToLoad('support/es5-shim-2.0.2/es5-shim.min.js');
+		addJSToLoad('support/es5-shim-2.0.2/es5-sham.min.js');
 		addJSToLoad('js/boot.js');
 
 		if (deploy) {
@@ -185,23 +185,23 @@ amber = (function() {
 
 	function loadDependencies() {
 		if (typeof jQuery == 'undefined') {
-			addJSToLoad('lib/jQuery/jquery-1.8.2.min.js');
+			addJSToLoad('support/jQuery/jquery-1.8.2.min.js');
 		}
 
 		if ((typeof jQuery == 'undefined') || (typeof jQuery.ui == 'undefined')) {
-			addJSToLoad('lib/jQuery/jquery-ui-1.8.16.custom.min.js');
+			addJSToLoad('support/jQuery/jquery-ui-1.8.16.custom.min.js');
 		}
 	}
 
 	function loadIDEDependencies() {
-		addJSToLoad('lib/jQuery/jquery.textarea.js');
-		addJSToLoad('lib/CodeMirror/codemirror.js');
-		addJSToLoad('lib/CodeMirror/smalltalk.js');
-		addJSToLoad('lib/CodeMirror/addon/hint/show-hint.js');
-		loadCSS('lib/CodeMirror/codemirror.css', '.');
-		loadCSS('lib/CodeMirror/theme/ambiance.css', '.');
-		loadCSS('lib/CodeMirror/addon/hint/show-hint.css', '.');
-		loadCSS('lib/CodeMirror/amber.css', '.');
+		addJSToLoad('support/jQuery/jquery.textarea.js');
+		addJSToLoad('support/CodeMirror/codemirror.js');
+		addJSToLoad('support/CodeMirror/smalltalk.js');
+		addJSToLoad('support/CodeMirror/addon/hint/show-hint.js');
+		loadCSS('support/CodeMirror/codemirror.css', '.');
+		loadCSS('support/CodeMirror/theme/ambiance.css', '.');
+		loadCSS('support/CodeMirror/addon/hint/show-hint.css', '.');
+		loadCSS('support/CodeMirror/amber.css', '.');
 	}
 
 	// This will be called after JS files have been loaded
