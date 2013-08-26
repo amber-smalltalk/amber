1d0bb3d09d7d1ba33f6fe7d275b552224437ab6f
Herbert Vojčík <herby@mailbox.sk>
2013-08-26 12:49:09 +0200

    amd as default transport for new packages

--- support/smalltalk.js
+++ support/smalltalk.js
@@ -1,6 +1,7 @@
 define("amber_vm/smalltalk", ["require", "module", "./boot"], function (require, module, boot) {
     var smalltalk = boot.smalltalk;
     smalltalk._amd_require = require;
+    smalltalk._defaultTransportType = "amd";
     var config = module.config && module.config();
     if (config) {
         smalltalk._amd_defaultNamespace = config.defaultNamespace;
