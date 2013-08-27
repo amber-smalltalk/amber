82fb2669e1624e2bb909c0bb4d7ea0cbbd4dad02
Herbert Vojčík <herby@mailbox.sk>
2013-08-27 14:12:04 +0200

    recompiled Test.js

--- test/Test.js
+++ test/Test.js
@@ -1,4 +1,4 @@
-define("amber_core/Test", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber/Kernel-Objects"], function(smalltalk,nil,_st){
+define("amber_core/Test", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_core/Kernel-Objects"], function(smalltalk,nil,_st){
 smalltalk.addPackage('Test');
 smalltalk.packages["Test"].transport = {"type":"amd","amdNamespace":"amber_core"};
 
