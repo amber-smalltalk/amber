25247895cbd610896b391e9b9f74ee8c19b95230
Herbert Vojčík <herby@mailbox.sk>
2013-11-09 18:24:35 +0100

    Allowing everything except actual npm publishing

--- cli/support/release.sh
+++ cli/support/release.sh
@@ -4,8 +4,8 @@
 # as a user with write access to amber npm package.
 
 cd `dirname "$0"`/../..
-echo rm -rf *
-echo git checkout master
+rm -rf *
+git checkout master
 echo -n "Which version are you going to publish [0 to skip]? "
 VER=`head -n 1`
 if [ "$VER" = "0" ]; then :; else
@@ -16,9 +16,8 @@ if [ "$VER" = "0" ]; then :; else
 	rm package.json.bak
 	git commit -a -m "Release version $VER"
 	git tag -a "$VER"
-#	echo bower upload
 # bower does not publish explicitly but implictly via semver tag
-	echo Please check if everything is ok, then publish the new release with 'npm publish'
+	echo npm publish
 fi
 echo -n "Which version are you going to work on? "
 VERF=`head -n 1`
@@ -29,6 +28,4 @@ cp package.json package.json.bak
 sed -e 's@/amber.git.*"@/amber.git"@' package.json.bak >package.json
 rm package.json.bak
 git commit -a -m "Working on $VERF"
-
-
-echo Please check if everything is ok, then push changes with 'git push --tags'
+git push --tags
