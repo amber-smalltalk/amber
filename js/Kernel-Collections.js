4b306c5a1b7d628a9ba76fcf74ae29a42afed291
Massimo Milan <massimo.milan@gmail.com>
2013-12-04 15:27:03 +0100

    Modified Bug fix to preserve speed

--- js/Kernel-Collections.js
+++ js/Kernel-Collections.js
@@ -3726,10 +3726,10 @@ category: 'copying',
 fn: function (aString){
 var self=this;
 return smalltalk.withContext(function($ctx1) { 
-return String(self) + aString._asString();
+return String(self) + aString;
 return self}, function($ctx1) {$ctx1.fill(self,",",{aString:aString},smalltalk.String)})},
 args: ["aString"],
-source: ", aString\x0a\x09<return String(self) + aString._asString()>",
+source: ", aString\x0a\x09<return String(self) + aString>",
 messageSends: [],
 referencedClasses: []
 }),
