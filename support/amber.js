20fc8a175dc0bd9eebfa1924cab2643268e01d25
Herbert Vojčík <herby@mailbox.sk>
2014-02-05 22:54:41 +0100

    rename callback function parameter to `amber`
    ... in comment example at the beginning of file

--- support/amber.js
+++ support/amber.js
@@ -3,11 +3,11 @@
     either defines 'require', thus passing config, if loaded prior require.js;
     or calls require.config, if loaded post require.js).
   Usage example:
-    require(['amber/devel'], function(smalltalk) {
-        smallralk.initialize({"transport.defaultAmdNamespace": "com_example_myproject"});
+    require(['amber/devel'], function(amber) {
+        amber.initialize({"transport.defaultAmdNamespace": "com_example_myproject"});
 
-        smalltalk.globals.Browser._open(); // for legacy IDE
-        smalltalk.popupHelios(); // for Helios IDE
+        amber.globals.Browser._open(); // for legacy IDE
+        amber.popupHelios(); // for Helios IDE
     });
   For detailed explanation of amber loading, see:
   https://github.com/amber-smalltalk/amber/wiki/How-to-load-amber
