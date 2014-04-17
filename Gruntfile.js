5c26044dd3ad0fcc78669c5e4f3eea84d481d78a
Herbert Vojčík <herby@mailbox.sk>
2014-04-17 16:38:23 +0200

    Trimmed gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('default', ['peg', 'amberc:all']);
+  grunt.registerTask('default', ['amberc:cli']);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -14,70 +14,17 @@ module.exports = function(grunt) {
       banner: '/*!\n <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> \n License: <%= pkg.license.type %> \n*/\n'
     },
 
-    peg: {
-      amber_parser: {
-        options: {
-          cache: true,
-          export_var: 'globals.SmalltalkParser'
-        },
-        src: 'support/parser.pegjs',
-        dest: 'support/parser.js',
-      }
-    },
-
     amberc: {
       options: {
         amber_dir: path.normalize(path.join(__dirname, "node_modules", "amber")),
         closure_jar: ''
-      },
-      all: {
-        output_dir : 'src',
-        src: ['src/Kernel-Objects.st', 'src/Kernel-Classes.st', 'src/Kernel-Methods.st', 'src/Kernel-Collections.st',
-              'src/Kernel-Infrastructure.st', 'src/Kernel-Exceptions.st', 'src/Kernel-Transcript.st', 'src/Kernel-Announcements.st',
-              'src/Kernel-ImportExport.st', 'src/Compiler-Exceptions.st', 'src/Compiler-Core.st', 'src/Compiler-AST.st',
-              'src/Compiler-IR.st', 'src/Compiler-Inlining.st', 'src/Compiler-Semantic.st', 'src/Compiler-Interpreter.st',
-              'src/Web.st', 'src/SUnit.st', 'src/IDE.st',
-              'src/Kernel-Tests.st', 'src/Compiler-Tests.st', 'src/SUnit-Tests.st',
-              'src/Helios-Core.st', 'src/Helios-Exceptions.st', 'src/Helios-Announcements.st',
-              'src/Helios-KeyBindings.st', 'src/Helios-Layout.st',
-              'src/Helios-Commands-Core.st', 'src/Helios-Commands-Tools.st', 'src/Helios-Commands-Browser.st',
-              'src/Helios-References.st', 'src/Helios-Inspector.st', 'src/Helios-Browser.st',
-              'src/Helios-Transcript.st', 'src/Helios-Workspace.st', 'src/Helios-Debugger.st',
-              'src/Helios-Workspace-Tests.st',
-              'src/Benchfib.st', 'src/Examples.st', 'src/Spaces.st'
-              ],
-        jsGlobals: ['navigator']
-      },
-      amber_kernel: {
-        output_dir : 'src',
-        src: ['src/Kernel-Objects.st', 'src/Kernel-Classes.st', 'src/Kernel-Methods.st', 'src/Kernel-Collections.st',
-              'src/Kernel-Infrastructure.st', 'src/Kernel-Exceptions.st', 'src/Kernel-Transcript.st', 'src/Kernel-Announcements.st']
-      },
-      amber_web: {
-        output_dir : 'src',
-        src: ['src/Web.st', 'src/SUnit.st']
-      },
-      amber_IDE: {
-        output_dir : 'src',
-        src: ['src/IDE.st'],
-        libraries: ['Web']
       },
       amber_tests: {
         output_dir : 'src',
         src: ['src/Kernel-Tests.st', 'src/Compiler-Tests.st', 'src/SUnit-Tests.st'],
         libraries: ['SUnit']
       },
-      amber_test_runner: {
-        src: ['test/Test.st'],
-        libraries: [
-        'Compiler-Exceptions', 'Compiler-Core', 'Compiler-AST',
-        'Compiler-IR', 'Compiler-Inlining', 'Compiler-Semantic', 'Compiler-Interpreter', 'parser',
-        'SUnit', 'Kernel-ImportExport',
-        'Kernel-Tests', 'Compiler-Tests', 'SUnit-Tests'],
-        main_class: 'NodeTestRunner',
-        output_name: 'test/amber_test_runner'
-      },
-      amber_cli: {
+      cli: {
         output_dir: 'cli/src',
         src: ['cli/src/AmberCli.st'],
         libraries: [
@@ -93,11 +40,7 @@ module.exports = function(grunt) {
     },
 
     jshint: {
-      amber: ['src/*.js'],
-      server: ['server/*.js'],
-      repl: ['repl/*.js'],
-      tests: ['test/*.js'],
-      grunt: ['grunt.js', 'grunt/**/*.js']
+      cli: ['cli/src/*.js', 'cli/support/*.js']
     }
   });
 };
