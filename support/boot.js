810fd964eea3d7bd19636d39dfbcf03ab51cd176
Herbert Vojčík <herby@mailbox.sk>
2014-08-20 16:44:37 +0200

    Console shim out of boot.js. Fixes #1065

--- support/boot.js
+++ support/boot.js
@@ -679,18 +679,6 @@ define("amber/boot", [ 'require', './browser-compatibility' ], function (require
 
 	function AugmentsBrik(brikz, st) {
 
-		/* Make sure that console is defined */
-
-		if(typeof console === "undefined") {
-			this.console = {
-				log: function() {},
-				warn: function() {},
-				info: function() {},
-				debug: function() {},
-				error: function() {}
-			};
-		}
-
 		/* Array extensions */
 
 		Array.prototype.addElement = function(el) {
