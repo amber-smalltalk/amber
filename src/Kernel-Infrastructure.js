3efae029431929e2e60f276501d830be93f1b9ec
Herbert Vojčík <herby@mailbox.sk>
2014-10-10 12:13:11 +0200

    Working on 0.14.0

--- src/Kernel-Infrastructure.js
+++ src/Kernel-Infrastructure.js
@@ -5075,12 +5075,12 @@ selector: "version",
 protocol: 'accessing',
 fn: function (){
 var self=this;
-return "0.13.0";
+return "0.14.0-pre";
 
 },
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: [],
-source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.13.0'",
+source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.14.0-pre'",
 referencedClasses: [],
 //>>excludeEnd("ide");
 messageSends: []
