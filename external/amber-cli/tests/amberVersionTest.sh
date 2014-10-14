db80861ae71d74d4cee9c2ad50aea1589720fbfa
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2014-10-14 20:20:35 +0200

    Revert "second implementation of bash script which tests amber version command"
    This reverts commit 63a0feb48fbbad8ead651912eeebf88d3526749e.

--- external/amber-cli/tests/amberVersionTest.sh
+++ external/amber-cli/tests/amberVersionTest.sh
@@ -1,29 +1,36 @@
-#!/bin/bash
-
-# Tests if the `amber version` command returns the expected amber version number, according to the configuration file `package.json`
-# Displays 'ok' in green if test succeeds, else 'not ok' in red.
-
-JSON_PACKAGE_PATH='../package.json' # {amber directory}/external/amber-cli/package.json
-AMBER_VERSION_COMMAND='../support/amber-cli.js version'
+PATH_JSON_PACKAGE="../package.json" # {amber directory}/external/amber-cli/pacakge.json
+NODE_COMMAND="../support/amber-cli.js version" # amber version
 
 RED="1;31"
 GREEN="1;32"
+BLUE="1;36"
 
-function makeItColorful {
+makeItColorFul(){
 	echo -e "\e[$2m$1\e[0m"
 }
 
-amberResult=`node $AMBER_VERSION_COMMAND`
+# GET VERSION #
+regexVersion='[0-9]\+\(\.[0-9]\+\)*[-_a-z]*' # regular expression used to find amber version
 
-expectedAmberVersion=`node --eval "console.log(require('./$JSON_PACKAGE_PATH').version)"`
+expectedAmberVersion=$(cat $PATH_JSON_PACKAGE | grep 'version' | grep -oe $regexVersion)
+expectedNodeVersion=$(node -v | grep -oe $regexVersion)
 
-# tests if expected amber version is in the result of `amber version` command
-echo $amberResult | grep $expectedAmberVersion > /dev/null 2>&1
+currentAmberVersion=$(node $NODE_COMMAND | grep -oe "version\ $regexVersion" | grep -oe $regexVersion)
+currentNodeVersion=$(node $NODE_COMMAND | grep -oe "NodeJS\ $regexVersion" | grep -oe $regexVersion)
+
+# display expected and current version
+makeItColorFul "\n\t Expected Amber version $expectedAmberVersion (current : $currentAmberVersion)" $BLUE
+makeItColorFul "\t Expected NodeJS version $expectedNodeVersion (current : $currentNodeVersion)" $BLUE
+
+# TEST AMBER VERSION #
+amberResult=$(node $NODE_COMMAND)
+
+echo $amberResult | grep $expectedAmberVersion > /dev/null 2>&1 && echo $amberResult | grep $expectedNodeVersion > /dev/null 2>&1 
+
 if [ $? -eq 0 ]
-then
-	makeItColorful "ok 1 - amber version" $GREEN
-	exit 0
-else
-	makeItColorful "not ok 1 - amber version\n\texpected : $expectedAmberVersion" $RED
-	exit 1
+then 
+	makeItColorFul "\n----------------------------\nSuccess - Test amber version\n" $GREEN && exit 0
+else 
+	makeItColorFul "\n----------------------------\nFailure - Test amber version\n" $RED && exit 1
 fi
+
