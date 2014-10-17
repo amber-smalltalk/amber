699ee154e9ed3fca0ed21fc64dedf30c68b1a329
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2014-10-17 09:13:16 +0200

    rewrite amberVersionTest.sh test using shell.js

---
+++ external/amber-cli/tests/amberVersionTest.js
@@ -0,0 +1,22 @@
+// Tests if the `amber version` command returns the expected amber version number, according to the configuration file `package.json`
+// Displays 'ok' in green if test succeeds, else 'not ok' in red.
+
+require('shelljs/global');
+require('colors');
+
+var JSON_PACKAGE_PATH = '../package.json'; // {amber directory}/external/amber-cli/package.json
+var AMBER_VERSION_COMMAND = './support/amber-cli.js version';
+
+var amberResult = exec("node " + AMBER_VERSION_COMMAND, {silent: true}).output;
+var expectedAmberVersion = require(JSON_PACKAGE_PATH).version;
+
+// tests if expected amber version is in the result of `amber version` command
+if (amberResult.indexOf(expectedAmberVersion) > -1) {
+	console.log("ok 1 - amber version".green);
+	exit(0);
+}
+else {
+	console.log(amberResult.red);
+	console.log(("not ok 1 - amber version\n\texpected : " + expectedAmberVersion).red);
+	exit(1);
+}
