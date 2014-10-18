b7b2db0905dfd0de8c5eee60c4bdacce3399fb12
Herbert Vojčík <herby@mailbox.sk>
2014-10-18 22:22:46 +0200

    helios: not exporting helios/set in .amd.json

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ var require;
 if (!require) require = {config: function (x) {require = x;}};
 require.config({
   "paths": {
-    "require-css": "bower_components/require-css",
-    "jquery": "bower_components/jquery/jquery",
     "jquery-ui": "bower_components/jquery-ui/jquery-ui.min",
-    "amber_lib/es5-shim": "bower_components/es5-shim",
+    "jquery": "bower_components/jquery/jquery",
     "amber_lib/codemirror": "bower_components/codemirror",
+    "amber_lib/es5-shim": "bower_components/es5-shim",
+    "require-css": "bower_components/require-css",
     "amber_lib/showdown": "my/helios/bower_components/showdown",
     "requireJS": "node_modules/requirejs/require",
     "bootstrap2.3.2": "my/helios/bower_components/bootstrap2.3.2/bootstrap",
@@ -17,20 +17,15 @@ require.config({
     "amber_vm/_st": "support/deprecated-vm-files/as-receiver",
     "amber_css": "support/resources",
     "amber_core": "src",
+    "amber_cli": "external/amber-cli/src",
     "helios": "my/helios/src",
-    "helios/set": "my/helios/set",
     "helios/resources": "my/helios/resources",
-    "helios/index": "my/helios/index",
-    "amber_cli": "external/amber-cli/src"
-  },
-  "map": {
-    "*": {
-      "css": "require-css/css"
-    }
+    "helios/index": "my/helios/index"
   },
   "shim": {
     "jquery-ui": {
       "deps": [
+        "jquery",
         "jquery",
         "jquery",
         "jquery"
@@ -39,17 +34,20 @@ require.config({
     "amber_lib/codemirror/lib/codemirror": {
       "deps": [
         "css!amber_lib/codemirror/lib/codemirror",
+        "css!amber_lib/codemirror/lib/codemirror",
         "css!amber_lib/codemirror/lib/codemirror"
       ]
     },
     "amber_lib/codemirror/mode/smalltalk/smalltalk": {
       "deps": [
         "../../lib/codemirror",
+        "../../lib/codemirror",
         "../../lib/codemirror"
       ]
     },
     "amber_lib/codemirror/addon/hint/show-hint": {
       "deps": [
+        "../../lib/codemirror",
         "../../lib/codemirror",
         "../../lib/codemirror"
       ]
@@ -62,6 +60,11 @@ require.config({
     },
     "ensure-console": {
       "exports": "console"
+    }
+  },
+  "map": {
+    "*": {
+      "css": "require-css/css"
     }
   },
   "rjs_comment1": "require.min should be removed",
