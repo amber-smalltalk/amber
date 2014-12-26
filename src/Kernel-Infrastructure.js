dbf3131935869df4972e440789898786632eef36
Herbert Vojčík <herby@mailbox.sk>
2014-12-26 12:53:11 +0100

    devkit: 0.3.0

--- src/Kernel-Infrastructure.js
+++ src/Kernel-Infrastructure.js
@@ -4942,12 +4942,12 @@ selector: "version",
 protocol: 'accessing',
 fn: function (){
 var self=this;
-return "0.14.0-pre";
+return "0.14.0-0";
 
 },
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: [],
-source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.14.0-pre'",
+source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.14.0-0'",
 referencedClasses: [],
 //>>excludeEnd("ide");
 messageSends: []
