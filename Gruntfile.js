0e097beb90976f9c48259c1ae948e590ddf42a00
Herbert Vojčík <herby@mailbox.sk>
2015-06-07 16:29:23 +0200

    Grunfile: formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,102 +1,102 @@
 var path = require('path');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.loadTasks('./internal/grunt-tasks');
-  grunt.loadTasks('./external/amber-dev/tasks');
+    grunt.loadTasks('./internal/grunt-tasks');
+    grunt.loadTasks('./external/amber-dev/tasks');
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-execute');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-execute');
 
-  grunt.registerTask('default', ['peg', 'amberc:all']);
-  grunt.registerTask('amberc:all', ['amberc:amber', 'amberc:cli', 'amberc:dev']);
-  grunt.registerTask('test', ['amberc:test_runner', 'execute:test_runner', 'clean:test_runner']);
-  grunt.registerTask('devel', ['amdconfig:amber']);
+    grunt.registerTask('default', ['peg', 'amberc:all']);
+    grunt.registerTask('amberc:all', ['amberc:amber', 'amberc:cli', 'amberc:dev']);
+    grunt.registerTask('test', ['amberc:test_runner', 'execute:test_runner', 'clean:test_runner']);
+    grunt.registerTask('devel', ['amdconfig:amber']);
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    meta: {
-      banner: '/*!\n <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> \n License: <%= pkg.license.type %> \n*/\n'
-    },
-
-    peg: {
-      parser: {
-        options: {
-          cache: true,
-          export_var: '$globals.SmalltalkParser'
+        meta: {
+            banner: '/*!\n <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> \n License: <%= pkg.license.type %> \n*/\n'
         },
-        src: 'support/parser.pegjs',
-        dest: 'support/parser.js'
-      }
-    },
 
-    amdconfig: {amber: {dest: 'config.js'}},
+        peg: {
+            parser: {
+                options: {
+                    cache: true,
+                    export_var: '$globals.SmalltalkParser'
+                },
+                src: 'support/parser.pegjs',
+                dest: 'support/parser.js'
+            }
+        },
 
-    amberc: {
-      options: {
-        amber_dir: process.cwd(),
-        closure_jar: ''
-      },
-      amber: {
-        output_dir : 'src',
-        src: ['src/Kernel-Objects.st', 'src/Kernel-Classes.st', 'src/Kernel-Methods.st', 'src/Kernel-Collections.st',
-              'src/Kernel-Infrastructure.st', 'src/Kernel-Exceptions.st', 'src/Kernel-Announcements.st',
-              'src/Platform-Services.st', 'src/Platform-ImportExport.st', 'src/Platform-Browser.st',
-              'src/Compiler-Exceptions.st', 'src/Compiler-Core.st', 'src/Compiler-AST.st',
-              'src/Compiler-IR.st', 'src/Compiler-Inlining.st', 'src/Compiler-Semantic.st', 'src/Compiler-Interpreter.st',
-              'src/SUnit.st',
-              'src/Kernel-Tests.st', 'src/Compiler-Tests.st', 'src/SUnit-Tests.st'
-              ],
-        jsGlobals: ['navigator']
-      },
-      cli: {
-        output_dir: 'external/amber-cli/src',
-        src: ['external/amber-cli/src/AmberCli.st'],
-        libraries: [
-          'Compiler-Exceptions', 'Compiler-Core', 'Compiler-AST',
-          'Compiler-IR', 'Compiler-Inlining', 'Compiler-Semantic', 'Compiler-Interpreter', 'parser',
-          'SUnit', 'Platform-ImportExport',
-          'Kernel-Tests', 'Compiler-Tests', 'SUnit-Tests'
-        ],
-        main_class: 'AmberCli',
-        output_name: '../support/amber-cli',
-        amd_namespace: 'amber_cli'
-      },
-      dev: {
-        output_dir: 'external/amber-dev/lib',
-        src: ['external/amber-dev/lib/NodeTestRunner.st'],
-        amd_namespace: 'amber_devkit'
-      },
-      test_runner: {
-        src: ['external/amber-dev/lib/NodeTestRunner.st'],
-        libraries: [
-        'Compiler-Exceptions', 'Compiler-Core', 'Compiler-AST',
-        'Compiler-IR', 'Compiler-Inlining', 'Compiler-Semantic', 'Compiler-Interpreter', 'parser',
-        'SUnit', 'Platform-ImportExport',
-        'Kernel-Tests', 'Compiler-Tests', 'SUnit-Tests'],
-        main_class: 'NodeTestRunner',
-        amd_namespace: 'amber_devkit',
-        output_name: 'test_runner'
-      }
-    },
+        amdconfig: {amber: {dest: 'config.js'}},
 
-    execute: {
-      test_runner: {
-        src: ['test_runner.js']
-      }
-    },
+        amberc: {
+            options: {
+                amber_dir: process.cwd(),
+                closure_jar: ''
+            },
+            amber: {
+                output_dir: 'src',
+                src: ['src/Kernel-Objects.st', 'src/Kernel-Classes.st', 'src/Kernel-Methods.st', 'src/Kernel-Collections.st',
+                    'src/Kernel-Infrastructure.st', 'src/Kernel-Exceptions.st', 'src/Kernel-Announcements.st',
+                    'src/Platform-Services.st', 'src/Platform-ImportExport.st', 'src/Platform-Browser.st',
+                    'src/Compiler-Exceptions.st', 'src/Compiler-Core.st', 'src/Compiler-AST.st',
+                    'src/Compiler-IR.st', 'src/Compiler-Inlining.st', 'src/Compiler-Semantic.st', 'src/Compiler-Interpreter.st',
+                    'src/SUnit.st',
+                    'src/Kernel-Tests.st', 'src/Compiler-Tests.st', 'src/SUnit-Tests.st'
+                ],
+                jsGlobals: ['navigator']
+            },
+            cli: {
+                output_dir: 'external/amber-cli/src',
+                src: ['external/amber-cli/src/AmberCli.st'],
+                libraries: [
+                    'Compiler-Exceptions', 'Compiler-Core', 'Compiler-AST',
+                    'Compiler-IR', 'Compiler-Inlining', 'Compiler-Semantic', 'Compiler-Interpreter', 'parser',
+                    'SUnit', 'Platform-ImportExport',
+                    'Kernel-Tests', 'Compiler-Tests', 'SUnit-Tests'
+                ],
+                main_class: 'AmberCli',
+                output_name: '../support/amber-cli',
+                amd_namespace: 'amber_cli'
+            },
+            dev: {
+                output_dir: 'external/amber-dev/lib',
+                src: ['external/amber-dev/lib/NodeTestRunner.st'],
+                amd_namespace: 'amber_devkit'
+            },
+            test_runner: {
+                src: ['external/amber-dev/lib/NodeTestRunner.st'],
+                libraries: [
+                    'Compiler-Exceptions', 'Compiler-Core', 'Compiler-AST',
+                    'Compiler-IR', 'Compiler-Inlining', 'Compiler-Semantic', 'Compiler-Interpreter', 'parser',
+                    'SUnit', 'Platform-ImportExport',
+                    'Kernel-Tests', 'Compiler-Tests', 'SUnit-Tests'],
+                main_class: 'NodeTestRunner',
+                amd_namespace: 'amber_devkit',
+                output_name: 'test_runner'
+            }
+        },
 
-    clean: {
-      test_runner: ['test_runner.js']
-    },
+        execute: {
+            test_runner: {
+                src: ['test_runner.js']
+            }
+        },
 
-    jshint: {
-      amber: ['src/*.js', 'support/[^p]*.js'],
-      cli: ['external/amber-cli/src/*.js', 'external/amber-cli/support/*.js'],
-      dev: ['external/amber-dev/lib/*.js'],
-      grunt: ['Gruntfile.js', 'internal/grunt-tasks/*.js', 'external/amber-dev/tasks/*.js']
-    }
-  });
+        clean: {
+            test_runner: ['test_runner.js']
+        },
+
+        jshint: {
+            amber: ['src/*.js', 'support/[^p]*.js'],
+            cli: ['external/amber-cli/src/*.js', 'external/amber-cli/support/*.js'],
+            dev: ['external/amber-dev/lib/*.js'],
+            grunt: ['Gruntfile.js', 'internal/grunt-tasks/*.js', 'external/amber-dev/tasks/*.js']
+        }
+    });
 };
