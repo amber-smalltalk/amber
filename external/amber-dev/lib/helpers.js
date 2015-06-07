80f3a2ed5b94b6e7ccc5b656b78834a683f8331f
Herbert Vojčík <herby@mailbox.sk>
2015-06-07 23:55:58 +0200

    devkit: fix shebang, add shebangless wrapping

--- external/amber-dev/lib/helpers.js
+++ external/amber-dev/lib/helpers.js
@@ -1,11 +1,18 @@
 exports.libPath = __dirname;
 
+exports.nodeShebang = "#!/usr/bin/env node";
+
 exports.nodeWrap = function (id) {
+    var wrap = exports.nodeWrapWithoutShebang(id);
+    wrap.start = exports.nodeShebang + "\n" + wrap.start;
+    return wrap;
+};
+
+exports.nodeWrapWithoutShebang = function (id) {
     var wrapSource = JSON.stringify('_wrap_' + id),
         idSource = JSON.stringify(id);
     return {
-        start: "#!/usr/bin/env/node\n" +
-        "(function(define){\n" +
+        start: "(function(define){\n" +
         "define(" + wrapSource + ", function (require) {\n",
         end: "require(" + idSource + ");\n" +
         "});\n" +
