a07a001ccad39ddb5816eb39d219a54d73be7fd3
Herbert Vojčík <herby@mailbox.sk>
2016-01-01 18:04:51 +0100

    Release version 0.14.18

--- src/Kernel-Infrastructure.js
+++ src/Kernel-Infrastructure.js
@@ -3419,12 +3419,12 @@ selector: "version",
 protocol: 'accessing',
 fn: function (){
 var self=this;
-return "0.15.0-pre";
+return "0.14.18";
 
 },
 //>>excludeStart("ide", pragmas.excludeIdeData);
 args: [],
-source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.15.0-pre'",
+source: "version\x0a\x09\x22Answer the version string of Amber\x22\x0a\x09\x0a\x09^ '0.14.18'",
 referencedClasses: [],
 //>>excludeEnd("ide");
 messageSends: []
