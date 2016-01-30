b07020a03f9ee4b10073ba93be141b63d505b22b
Herbert Vojčík <herby@mailbox.sk>
2016-01-30 23:28:48 +0100

    Reminder of browser-compatibility hack and need of no define(...).

--- support/browser-compatibility.js
+++ support/browser-compatibility.js
@@ -1,3 +1,6 @@
+// This file is injected dependencies by amber-compat-xxx modules.
+// Therefore it is important that it _does_not_have_ define call.
+
 /* Make sure that console is defined */
 if(typeof console === "undefined") {
 	this.console = {
